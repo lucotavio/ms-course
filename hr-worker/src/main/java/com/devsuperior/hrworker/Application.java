a3897c965ba3dc53716cb8d4fc5d0449c71d9517
Luciano <luc.oliveira343@gmail.com>
2022-07-05 17:33:11 -0300

    hr-worker implementation

--- hr-worker/src/main/java/com/devsuperior/hrworker/Application.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrWorkerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,36 @@
+package com.devsuperior.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_WORKER")
+public class Worker implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Column(name = "DAILY_INCOME")
+	private Double dailyIncome;
+}
