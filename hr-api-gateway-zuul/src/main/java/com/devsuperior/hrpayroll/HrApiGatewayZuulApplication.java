b955467e29dbeabc05060d142aa5e7fa0a50e37d
Luciano <luc.oliveira343@gmail.com>
2022-07-05 22:45:38 -0300

    Feign Client

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrpayroll/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrpayroll/HrApiGatewayZuulApplication.java
@@ -2,14 +2,12 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
-public class Application {
+public class HrApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
 
 }
